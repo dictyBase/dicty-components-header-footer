5b15d2689b2f6d098d56d2e26eef78a41d107c87
Siddhartha Basu <biosidd@gmail.com>
2017-07-12 12:57:34 +0000

    Added searchbar styled component

---
+++ src/styles/SearchBar.js
@@ -0,0 +1,22 @@
+import React from "react"
+import styled from "styled-components"
+
+const SearchBarStyle = styled.input`
+  cursor: not-allowed;
+  border: 1px solid rgb(204, 204, 204);
+  border-top-right-radius: 0px;
+  border-bottom-right-radius: 0px;
+  border-top-left-radius: 3px;
+  border-bottom-left-radius: 3px;
+  width: 50%;
+  height: 30px;
+  color: #555;
+  padding: 0px 0px 0px 5px;
+`
+
+const SearchBar = ({ children, ...props }) =>
+  <SearchBarStyle {...props}>
+    {children}
+  </SearchBarStyle>
+
+export default SearchBarStyle
