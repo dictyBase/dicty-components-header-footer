25e291679e29b318441e0a2c0ddc41cd516d0c09
Siddhartha Basu <biosidd@gmail.com>
2017-07-20 20:56:35 +0000

    Added options to pass various space properties

--- src/styles/footer/FooterItemsHeader.js
+++ src/styles/footer/FooterItemsHeader.js
@@ -1,10 +1,14 @@
 import React from "react"
 import styled from "styled-components"
+import { space, fontSize, color } from "styled-system"
 
 const ContainerStyle = styled.div`
   @media (max-width: 40em) : {
     textAlign: center;
   }
+  ${space};
+  ${fontSize};
+  ${color};
 `
 
 const FooterItemsHeader = ({ children, ...props }) =>
