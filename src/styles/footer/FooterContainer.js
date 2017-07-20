2b4d6cc4998a57ca123f680eabf7a4458c6c8667
Siddhartha Basu <biosidd@gmail.com>
2017-07-20 21:05:40 +0000

    footer items are arranged along the center line

--- src/styles/footer/FooterContainer.js
+++ src/styles/footer/FooterContainer.js
@@ -3,6 +3,7 @@ import { Flex } from "rebass"
 import styled from "styled-components"
 
 const ContainerStyle = styled(Flex)`
+  justify-content: center;
   background: ${props =>
     props.theme.background
       ? props.theme.background
