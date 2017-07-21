71ba8aed07fdfc70632cdcc8fdcb476b3b472827
Siddhartha Basu <biosidd@gmail.com>
2017-07-21 00:53:26 +0000

    Refactored the fake footer data in a separate file

--- demo/src/index.js
+++ demo/src/index.js
@@ -3,65 +3,7 @@ import { render } from "react-dom"
 import { Flex, Box, Divider } from "rebass"
 
 import { Header, Footer } from "../../src"
-
-const items = [
-  [
-    {
-      header: { link: "http://yahoo.com", description: "Genomes" },
-      items: [{ link: "http://cnn.com", description: "Genomes Home" }],
-    },
-  ],
-  [
-    {
-      header: { description: "Tools", link: "http://dictybase.org/tools" },
-      items: [
-        { link: "http://dictybase.org/tools", description: "Tools home" },
-        {
-          link: "http://dictybase.org/tools",
-          description: "New Genomes browser",
-        },
-      ],
-    },
-  ],
-  [
-    {
-      header: { description: "Research", link: "http://dictybase.org/tools" },
-      items: [
-        { link: "http://dictybase.org/tools", description: "Research home" },
-        { link: "http://dictybase.org/tools", description: "Anatomy ontology" },
-        { link: "http://dictybase.org/tools", description: "Codon bias table" },
-        {
-          link: "http://dictybase.org/tools",
-          description: "Nomenclature Guidelines",
-        },
-        { link: "http://dictybase.org/tools", description: "Phenotyping" },
-      ],
-    },
-  ],
-  [
-    {
-      header: { description: "Please Cite", link: "http://dictybase.org/cite" },
-      items: [
-        { description: "dictyBase", link: "http://dictybase.org" },
-        {
-          description: "Dicty stock center",
-          link: "http://dictybase.org/stock",
-        },
-      ],
-    },
-    {
-      header: {
-        description: "Supported By",
-        link: "http://dictybase.org/support",
-      },
-      items: [
-        { description: "NIH", link: "http://dictybase.org/nih" },
-        { description: "GMOD", link: "http://dictybase.org/nih" },
-        { description: "Gene ontology", link: "http://dictybase.org/nih" },
-      ],
-    },
-  ],
-]
+import items from "../../src/data/footer"
 
 const Demo = () =>
   <Flex column>
