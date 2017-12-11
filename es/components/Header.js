ca112d34cacc804a4687d5a14c719119d09db4ca
wildlifehexagon <crossthebreeze@gmail.com>
2017-12-11 14:22:29 -0600

    update snapshots + rebuild libraries

--- es/components/Header.js
+++ es/components/Header.js
@@ -8,17 +8,37 @@ import "font-awesome/css/font-awesome.min.css";
 import FontAwesome from "react-fontawesome";
 
 var Home = "http://dictybase.org";
+// type HeaderProps = {
+//   /** Link for the download page */
+//   downloads: string,
+//   /** Link for the information page */
+//   info: string,
+//   /** Link for the citation page */
+//   cite: string,
+//   /** Link for the login page */
+//   login: string,
+//   /** Link for the logo, that goes to the home page */
+//   home?: string,
+// }
 
+var headerItems = function headerItems(items) {
+  return items.map(function (link, i) {
+    return React.createElement(
+      Link,
+      { key: i, href: link.url },
+      React.createElement(FontAwesome, { name: link.icon }),
+      " ",
+      link.text
+    );
+  });
+};
 
 /**
  * The `Header` component that will be displayed
  * on top of navigation bar in every react web application of [dictyBase](http://dictybase.org).
  */
 var Header = function Header(_ref) {
-  var downloads = _ref.downloads,
-      info = _ref.info,
-      cite = _ref.cite,
-      login = _ref.login,
+  var items = _ref.items,
       _ref$home = _ref.home,
       home = _ref$home === undefined ? Home : _ref$home;
 
@@ -40,30 +60,7 @@ var Header = function Header(_ref) {
       React.createElement(
         LinksContainer,
         null,
-        React.createElement(
-          Link,
-          { href: login },
-          React.createElement(FontAwesome, { name: "sign-in" }),
-          " Login"
-        ),
-        React.createElement(
-          Link,
-          { href: cite },
-          React.createElement(FontAwesome, { name: "plus" }),
-          " Cite Us"
-        ),
-        React.createElement(
-          Link,
-          { href: downloads },
-          React.createElement(FontAwesome, { name: "download" }),
-          " Downloads"
-        ),
-        React.createElement(
-          Link,
-          { href: info },
-          React.createElement(FontAwesome, { name: "info-circle" }),
-          " About dictyBase"
-        )
+        headerItems(items)
       ),
       React.createElement(
         SearchContainer,
