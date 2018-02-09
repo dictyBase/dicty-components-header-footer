bc03c7d91b7ef5613b9a084d4c4814f249085ad3
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2018-02-09 22:55:22 +0000

    updated library build

--- es/components/Header.test.js
+++ es/components/Header.test.js
@@ -2,6 +2,7 @@ import React from "react"
 import Header from "./Header"
 import renderer from "react-test-renderer"
 import items from "../data/header"
+import "jest-styled-components"
 
 test("matching a snapshot of Header", () => {
   const component = renderer.create(<Header items={items} />)
---
+++ es/data/header.js
@@ -0,0 +1,17 @@
+module.exports = [{
+  url: "/login",
+  icon: "sign-in",
+  text: "Login"
+}, {
+  url: "/cite",
+  icon: "plus",
+  text: "Cite Us"
+}, {
+  url: "/downloads",
+  icon: "download",
+  text: "Downloads"
+}, {
+  url: "/info",
+  icon: "info-circle",
+  text: "About dictyBase"
+}];
