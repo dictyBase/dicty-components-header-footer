0dd6ca041ca234a1f719adfbbb62f4c111667808
wildlifehexagon <hartline.eric@gmail.com>
2021-04-10 13:06:11 -0500

    refactor: add old footer design back for demo purposes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,41 +1,63 @@
 import React from "react"
 import { makeStyles, Theme } from "@material-ui/core/styles"
 import Grid from "@material-ui/core/Grid"
-import Divider from "@material-ui/core/Divider"
-import Typography from "@material-ui/core/Typography"
 
 const useStyles = makeStyles((theme: Theme) => ({
-  container: {
-    backgroundColor: "#edf2f7",
+  root: {
+    width: "100%",
+    background: "#004080",
+    paddingRight: 20,
+    paddingLeft: 20,
+    paddingBottom: 10,
   },
-  gridItem: {
-    [theme.breakpoints.down("xs")]: {
-      width: "50%",
+  listItem: {
+    margin: 0,
+    padding: 1,
+    listStyle: "none",
+  },
+  headerLink: {
+    textAlign: "left",
+    padding: 0,
+  },
+  subsectionItem: {
+    paddingLeft: 15,
+    paddingRight: 15,
+  },
+  link: {
+    color: "#d8d8d8",
+    fontSize: 11,
+    textDecoration: "none",
+    "&:hover": {
+      textDecoration: "underline",
     },
-    paddingLeft: theme.spacing(2),
-    paddingRight: theme.spacing(2),
   },
-  list: {
-    padding: "0px",
-  },
-  listHeader: {
-    color: "#004080",
-    fontSize: "1.2em",
+  ulHeader: {
+    color: "#ebe97a",
+    fontSize: 14,
     textDecoration: "none",
     listStyle: "none",
     marginBottom: theme.spacing(1),
   },
-  listItem: {
-    margin: "0px",
-    padding: "0px",
-    listStyle: "none",
+  // center text for all items/links at sm/xs breakpoints
+  [theme.breakpoints.down("sm")]: {
+    listItem: {
+      textAlign: "center",
+    },
+    headerLink: {
+      textAlign: "center",
+      padding: 0,
+    },
   },
-  link: {
-    color: "#374151",
-    fontSize: "0.8em",
-    textDecoration: "none",
-    "&:hover": {
-      color: "#004080",
+  // tighten up spacing for xs breakpoints
+  [theme.breakpoints.down("xs")]: {
+    root: {
+      padding: 0,
+    },
+    gridItem: {
+      width: "50%",
+    },
+    subsectionItem: {
+      paddingLeft: 0,
     },
   },
 }))
@@ -69,19 +91,21 @@ const Footer = ({ items }: FooterProps) => {
 
   const footerItems = (items: Array<ItemType>) =>
     items.map((c, i) => (
-      <Typography key={i}>
+      <div key={i} className={classes.listItem}>
         <a href={c.link} className={classes.link}>
           {c.description}
         </a>
-      </Typography>
+      </div>
     ))
 
   const footerSubSections = (items: Array<FooterItemType>) =>
     items.map((c, i) => (
-      <ul key={i} className={classes.list}>
-        <li className={classes.listHeader}>{c.header.description}</li>
-        <li className={classes.listItem}>{footerItems(c.items)}</li>
-      </ul>
+      <div key={i} className={classes.subsectionItem}>
+        <ul className={classes.headerLink}>
+          <li className={classes.ulHeader}>{c.header.description}</li>
+          <li className={classes.listItem}>{footerItems(c.items)}</li>
+        </ul>
+      </div>
     ))
 
   const footerSections = (items: Array<Array<FooterItemType>>) =>
@@ -92,12 +116,9 @@ const Footer = ({ items }: FooterProps) => {
     ))
 
   return (
-    <React.Fragment>
-      <Divider />
-      <Grid container justify="center" className={classes.container}>
-        {footerSections(items)}
-      </Grid>
-    </React.Fragment>
+    <Grid container className={classes.root} justify="center">
+      {footerSections(items)}
+    </Grid>
   )
 }
 
