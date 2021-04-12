8f7a020be46b0f44582100ccc0e1956498ba3a1e
wildlifehexagon <hartline.eric@gmail.com>
2021-04-12 15:15:17 -0500

    refactor: move types into separate file

--- dist/components/Footer.d.ts
+++ dist/components/Footer.d.ts
@@ -1,29 +1,10 @@
 /// <reference types="react" />
-declare type FooterLink = {
-    /** url link */
-    link?: string;
-    /** description of the link that will be displayed */
-    description?: string;
-};
-declare type FooterItem = {
-    /** The header or title of every footer section */
-    header: FooterLink;
-    /** List of entry under the header */
-    items: Array<FooterLink>;
-};
-declare type Theme = {
-    /** RGB, hex or CSS color string used as background color for footer and hovered links */
-    primary: string;
-    /** RGB, hex or CSS color string used for list headers */
-    secondary: string;
-    /** RGB, hex or CSS color string used as primary text color */
-    text: string;
-};
+import { FooterItem, Theme } from "../types";
 declare type Props = {
     /** Colors used as part of footer theme */
     theme: Theme;
     /** List of items to display in footer */
-    items: Array<Array<FooterItem>>;
+    items: FooterItem[][];
 };
 declare const Footer: ({ items, theme }: Props) => JSX.Element;
 export default Footer;
--- src/types/index.ts
+++ src/types/index.ts
@@ -9,4 +9,27 @@ type HeaderItem = {
   text: string
 }
 
-export type { HeaderItem }
+type FooterLink = {
+  /** url link */
+  link?: string
+  /** description of the link that will be displayed */
+  description?: string
+}
+
+type FooterItem = {
+  /** The header or title of every footer section */
+  header: FooterLink
+  /** List of entry under the header */
+  items: FooterLink[]
+}
+
+type Theme = {
+  /** RGB, hex or CSS color string used as background color for footer and hovered links */
+  primary: string
+  /** RGB, hex or CSS color string used for list headers */
+  secondary: string
+  /** RGB, hex or CSS color string used as primary text color */
+  text: string
+}
+
+export type { HeaderItem, FooterItem, FooterLink, Theme }
