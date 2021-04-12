5b9951e25b540db717f062e0d9e9a4a68019dcd6
wildlifehexagon <hartline.eric@gmail.com>
2021-04-12 09:35:36 -0500

    feat: export site map footer for testing

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,4 @@
 export { default as Header } from "./components/Header";
 export { default as Footer } from "./components/Footer";
 export { default as HeaderLink } from "./components/HeaderLink";
+export { default as SiteMapFooter } from "./components/SiteMapFooter";
