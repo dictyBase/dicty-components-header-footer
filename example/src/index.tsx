63817818e9c4feb827ccba8d4478531f0589a256
wildlifehexagon <hartline.eric@gmail.com>
2021-04-13 12:13:38 -0500

    refactor: add css baseline to example app

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -1,10 +1,12 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import CssBaseline from "@material-ui/core/CssBaseline"
 import App from "./App"
 import "@fontsource/roboto"
 
 ReactDOM.render(
   <React.StrictMode>
+    <CssBaseline />
     <App />
   </React.StrictMode>,
   document.getElementById("root"),
