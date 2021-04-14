3c4292464f36a1f32b0adbfeebd3c4cfbab2f897
wildlifehexagon <hartline.eric@gmail.com>
2021-04-14 09:58:54 -0500

    refactor: simplify styling in condensed footer

--- example/src/CondensedFooter.tsx
+++ example/src/CondensedFooter.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import { makeStyles, Theme } from "@material-ui/core/styles"
+import Box from "@material-ui/core/Box"
 import Grid from "@material-ui/core/Grid"
 import Typography from "@material-ui/core/Typography"
 
@@ -43,19 +44,6 @@ const links = [
 ]
 
 const useStyles = makeStyles((theme: Theme) => ({
-  container: {
-    marginTop: theme.spacing(2),
-    marginBottom: theme.spacing(2),
-    paddingLeft: theme.spacing(2),
-    paddingRight: theme.spacing(2),
-    backgroundColor: "#004080",
-  },
-  header: {
-    marginTop: theme.spacing(2),
-    marginBottom: theme.spacing(2),
-    color: "#ebe97a",
-    textAlign: "center",
-  },
   link: {
     color: "#d8d8d8",
     paddingRight: theme.spacing(0.5),
@@ -67,15 +55,6 @@ const useStyles = makeStyles((theme: Theme) => ({
       },
     },
   },
-  support: {
-    marginTop: theme.spacing(1),
-  },
-  supportedBy: {
-    color: "#d8d8d8",
-    marginTop: theme.spacing(1),
-    marginBottom: theme.spacing(2),
-    marginRight: theme.spacing(1),
-  },
 }))
 
 const CondensedFooter = () => {
@@ -83,27 +62,31 @@ const CondensedFooter = () => {
 
   return (
     <footer>
-      <Grid container justify="center" className={classes.container}>
-        <Grid item xs={12} className={classes.header}>
-          <Typography variant="h6">Dicty Community Resource</Typography>
+      <Box my={2} px={2} bgcolor="#004080">
+        <Grid container justify="center">
+          <Grid item xs={12}>
+            <Box my={2} color="#ebe07a" textAlign="center">
+              <Typography variant="h6">Dicty Community Resource</Typography>
+            </Box>
+          </Grid>
+          <Grid item container justify="center">
+            {links.map((item, index) => {
+              const separator = index ? " • " : ""
+              return (
+                <span key={index} className={classes.link}>
+                  {separator}
+                  <a href={item.url}>{item.description}</a>
+                </span>
+              )
+            })}
+          </Grid>
+          <Grid item xs={12} container justify="flex-end">
+            <Box mt={1} mb={2} mr={1} color="#d8d8d8">
+              <em>Supported by NIH/NIGMS</em>
+            </Box>
+          </Grid>
         </Grid>
-        <Grid item container justify="center">
-          {links.map((item, index) => {
-            const separator = index ? " • " : ""
-            return (
-              <span key={index} className={classes.link}>
-                {separator}
-                <a href={item.url}>{item.description}</a>
-              </span>
-            )
-          })}
-        </Grid>
-        <Grid item xs={12} container justify="flex-end">
-          <span className={classes.supportedBy}>
-            <em>Supported by NIH/NIGMS</em>
-          </span>
-        </Grid>
-      </Grid>
+      </Box>
     </footer>
   )
 }
